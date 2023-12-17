c1cb653e1ad579d8ee6baebc402aafe936c3c6a3
Diego Massarni <webdiegomassarini@gmail.com>
2023-12-18 00:48:03 +0100

    first commit

---
+++ sanity/sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: "diego's blog",
+
+  projectId: 'bkse6yvk',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
